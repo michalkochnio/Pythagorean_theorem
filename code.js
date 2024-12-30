94b3644a081dfebb322857164de2ffc38dbfa8d8
Michal Kochnio <kochniomichal@gmail.com>
2024-12-30 13:44:56 +0100

    Operation

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
-let a = window.prompt("Length of thecathetus A?");
-let b = window.prompt("Length of thecathetus B?");
+let a = window.prompt("Length of the cathetus A?");
+let b = window.prompt("Length of the cathetus B?");
 
 a = Number(a);
 b = Number(b);
+
+let hyptoneuseVal = (a * a + b * b) ** 0.5;
+console.log(hyptoneuseVal);
